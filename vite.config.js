730780f627d4850e9c63364b3e602603574854f2
Xiaowei0122 <345776840@qq.com>
2026-05-18 11:33:26 +0800

    fix：前后端容器构建分离，修改代理和容器内访问地址 新增dockerfile.bak 测试部署ver0.1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '192.168.1.170', // 允许局域网访问
+    host: '0.0.0.0', // 允许局域网访问
     port: 5173,       // Vite 默认端口
   },
 })
